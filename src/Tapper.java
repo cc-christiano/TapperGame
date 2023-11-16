0f1d4c8ec3332323751e58b50259fb6fb3e41e4e
cc-christiano <charlotte_christiano25@milton.edu>
2023-11-16 08:19:17 -0500

    Initial commit

---
+++ src/Tapper.java
@@ -0,0 +1,9 @@
+public class Tapper {
+
+    public int numCustomers;
+    public String color;
+    public boolean wantDrink;
+    public boolean moveUP;
+    public boolean moveDown;
+    public int speed;
+}
